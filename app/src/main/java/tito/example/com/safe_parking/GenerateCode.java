c6cb84a3e6ebb0c0765287968f2c9f095ac022d0
TitoNadar <titonadar06@gmail.com>
2018-03-20 16:06:42 +0530

    deprndencies set

---
+++ app/src/main/java/tito/example/com/safe_parking/GenerateCode.java
@@ -0,0 +1,14 @@
+package tito.example.com.safe_parking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GenerateCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_code);
+
+    }
+}
